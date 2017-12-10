330c9d2a931146cb3ebf677607f60c5910fb52b2
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2017-12-10 19:55:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuss(number)
-  if number % 5 && number % 3
+  if number % 5 && number % 3 
     "Fizzbuzz"
-  elsif number % 3
+  elsif number % 3 
     "Fizz"
   elsif number % 5
     "Buzz"
