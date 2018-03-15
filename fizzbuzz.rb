1739836da00b35d8569668578a02dab4829705c5
Ryan Hu <ryanhuuu@gmail.com>
2018-03-15 23:02:23 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
   if (num%5==0 && num%3==0)
     return "FizzBuzz"
-  else if (num%5==0)
+  elsif (num%5==0)
     return "Buzz"
-  else if (num%3==0)
+  elsif (num%3==0)
     return "Fizz"
   end
 end
