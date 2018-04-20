d3735daadd8566c1c1932d5919343d410bed8782
github username <github email address>
2018-04-19 20:23:42 -0700

    Edited #fizzbuzz conditional order

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 0 && 5 == 0
+  "FizzBuzz"
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 == 0 && 5 == 0
-    "FizzBuzz"
   end
 end
