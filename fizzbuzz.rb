1d0b2aaefc4f9096459cebe49c42b11b5b9caecf
Laura Adam <laurawynosky@gmail.com>
2018-05-28 21:09:19 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 #play fizzbuzz
 
 def fizzbuzz(int)
-  if int%3 $$ int%5 == 0
-  return "FizzBuzz"
+  if int (% 3 == 0) $$ int (% 5 == 0)
+    puts "FizzBuzz"
   end
   
-  if int %5 ==0 
-  return "Buzz"
+  if int (% 5 == 0) 
+    puts "Buzz"
   end
 
-  if int %3 ==0 
-  return "Fizz"
+  if int (% 3 == 0) 
+    puts "Fizz"
   end
 end
