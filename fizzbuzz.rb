d937e8921209bec509dd839324633575b07b0d21
Laura Adam <laurawynosky@gmail.com>
2018-05-28 20:51:46 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+#play fizzbuzz
+
+def fizzbuzz(int)
+  if int%3 $$ int%5 == 0
+  return "FizzBuzz"
+  end
+  
+  if int %5 ==0 
+  return "Buzz"
+  end
+
+  if int %3 ==0 
+  return "Fizz"
+  end
+end
