827b2f1da11c00e332aa1c7ef0d6612cb91ff377
Bryan Teng <bteng1226@gmail.com>
2018-06-23 05:44:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@ def fizzbuzz(int)
   value.push("Fizz") if int%3 == 0
   value.push("Buzz") if int%5 == 0
   
-  value.length == 0 ? value.push(nil) : value.join
+  value.length == 0 ? value = nil : value = value.join
   
   value
 end
