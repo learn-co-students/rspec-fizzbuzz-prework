587a33395888a45e215136589235bf9ecd9e3ff1
Rosa Tu <rosalina.tu@gmail.com>
2018-06-26 15:57:11 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,13 @@
-def fizzbuzz
-  
+def fizzbuzz(num)
+  if
+    num % 3 == 0 
+    return "Fizz"
+  elsif
+    num % 5 == 0 
+    return "Buzz"
+  elsif
+    num % 15 == 0
+    return "FizzBuzz"
+  else  
+    return nil
 end  
