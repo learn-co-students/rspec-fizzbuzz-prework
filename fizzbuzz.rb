04e797f2b9c235fb34010209a098208b6e011772
Rosa Tu <rosalina.tu@gmail.com>
2018-06-26 16:00:04 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(num)
   if
-    num % 3 == 0 
+    (num % 3 == 0) && (num % 5 != 0) 
     return "Fizz"
   elsif
-    num % 5 == 0 
+    (num % 5 == 0) && (num % 3 != 0)
     return "Buzz"
   elsif
-    num % 15 == 0
+    (num % 5 == 0) && (num % 3 == 0)
     return "FizzBuzz"
   else  
     return nil
