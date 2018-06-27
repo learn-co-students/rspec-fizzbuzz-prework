2989b1b3667377a095afb4fc8ef3a635aa7d282f
Ramona M <r1mari@yahoo.com>
2018-06-27 12:35:07 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(n)
+
+  if n % 3 == 0 && n % 5 == 0 
+    return 'FizzBuzz'
+  
+  elsif n % 3 == 0 
+    return 'Fizz'
+    
+  elsif n % 5 == 0 
+    return 'Buzz'
+    
+  else
+    print n
+  end
+  
+end
+  
