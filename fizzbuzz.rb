8f18ece3f41e7287ebaaed938442662be892a174
github username <github email address>
2018-07-02 14:06:35 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,9 @@
 def fizzbuzz(num)
-  if num % 3 == 0
-      if num % 5 == 0
-        return "FizzBuzz"
-      end
-    return "Fizz"
+  if num % 3 == 0 && num % 5 == 0
+      return "FizzBuzz"
+  elsif num % 3 == 0
+      return "Fizz"
   elsif num % 5 == 0
       return "Buzz"
-  else
-      puts 
   end
 end
