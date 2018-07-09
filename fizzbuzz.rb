9b54224d5d2f9d1bacc84ef47434053f98a1b3dd
Daniel Walz <danielwalz@Daniels-MacBook-Pro.local>
2018-07-09 06:54:04 -0500

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+
+def fizzbuzz(number)
+
+  if number % 3 == 0 && number % 5 != 0
+    return "Fizz"
+  elsif number % 5 == 0 && number % 3 != 0
+    return "Buzz"
+  elsif number % 3 && number % 5 == 0
+    return "FizzBuzz"
+  else
+    return nil
+
+  end
+end
