3062a8a6a86c4a9bcf4fb514edd925bb6c681dcb
Frida Casas <fridakahsas@yahoo.com>
2018-08-19 16:18:07 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(n)
+  if n % 5 == 0 && n % 3 == 0
+    return "FizzBuzz"
+  else if n % 3 == 0 
+    return "Fizz"
+  else if n % 5 == 0
+    return "Buzz"
+  else 
+    return nil
+  end
+end
+end
+end
+    
