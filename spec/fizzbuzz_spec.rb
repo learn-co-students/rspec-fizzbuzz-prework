63285bcedc347b2079ff897093d4e7e2bda97a4e
FutureScript <fernandobaxter1@gmail.com>
2018-09-14 18:41:18 +0000

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,6 +4,7 @@ require_relative '../fizzbuzz.rb'
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
+    
 
     expect(fizz_3).to eq("Fizz")
   end
