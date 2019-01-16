493c528fc07cb643c41f59e155f932c8e66767cb
ahkian <dbrazel4@gmail.com>
2019-01-16 16:27:20 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 != 0
-    "Fizz"
-  elsif number % 3 != 0 && number % 5 == 0
-    "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
-    "FizzBuzz"
+  if number % 3 == 0 && number % 5 == 0
+    'FizzBuzz'
+  elsif number % 3 == 0
+    'Fizz'
+  elsif number % 5 == 0
+    'Buzz'
   end
 end
