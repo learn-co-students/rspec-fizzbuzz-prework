6e88d814ba4a0186dd79a9209d4025c530b53619
Errol W <ewatsonrei@gmail.com>
2019-02-02 19:12:08 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,17 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  #elsif int % 3 == 0 && int % 5 == 0
-  elsif int % 15 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
   else
     return nil
   end
 end
+
+#elsif int % 3 == 0 || int % 5 == 0
+#elsif int % 15 == 0
+
+#elsif int % 15 != 0
+  #  return nil
+  #else
+  #  return "FizzBuzz"
