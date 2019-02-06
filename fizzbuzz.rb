0f461a1683171dbb26b2c3ee6ac78d0223ea15a3
ko kristin <koberlander115@gmail.com>
2019-02-06 18:49:05 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,26 @@
+def fizzbuzz(int)
+  if int % 5 === 0 && int % 3 === 0
+    "FizzBuzz"
+    elsif int % 3 === 0
+       "Fizz"
+      elsif int % 5 === 0
+         "Buzz"
+  else 
+    return nil
+  end
+end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# fizzbuzz(3) == "Fizz"
