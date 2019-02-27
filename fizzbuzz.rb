efe46ef1971fc37577be04b374258de3e9f995ec
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-02-27 20:54:53 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 def fizzbuzz(num)
   if num%3 == 0 && num%5 == 0
     return "FizzBuzz"
-  end
-  if num % 3 == 0
+  elsif num % 3 == 0
     return "Fizz"
-  end
-  if num % 5 == 0
+  elsif num % 5 == 0
     return "Buzz"
   end
 end
