ca5f248d4475308af80086cc66b91d26db612621
Devin Arnold <dbarnold321@gmail.com>
2019-04-30 23:57:39 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,10 @@
 def fizzbuzz (number)
-  if number == 15
+  if number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
   elsif number % 3 == 0
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-  elsif number % 5 > 0
-    return nil
-  elsif number % 3 > 0
-    return nil
-  else
-    return "nil"
 
   end
 end
