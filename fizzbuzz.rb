9d60c3168ab36207ff84be1017b8c248dc5dc885
github username <github email address>
2019-06-20 21:07:00 -0500

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+
+def fizzbuzz(num)
+    if num % 5 == 0 && num % 3 == 0
+        "FizzBuzz"
+    elsif num % 3 == 0
+        "Fizz"
+    elsif num % 5 == 0
+        "Buzz"
+    else 
+       
+    end
+end
